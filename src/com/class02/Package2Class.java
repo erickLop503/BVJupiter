acdb05f8edf40eec5b3926b5e33173a3803aff08
erickLop503 <4everklopez@gmail.com>
2020-03-19 19:40:42 -0400

    second packge testcase

---
+++ src/com/class02/Package2Class.java
@@ -0,0 +1,11 @@
+package com.class02;
+
+public class Package2Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("test case 1");
+	}
+
+}
